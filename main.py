1bfadb51773ab8052f4800f7201438e2900d0b51
Erik Gonzalez <erikhuboy@gmail.com>
2021-10-25 21:18:46 -0500

    fix none filter

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Auto:
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
-        self.asientos = [asiento for asiento in asientos if asiento]
+        self.asientos = [asiento for asiento in asientos if asiento != None]
         self.marca = marca
         self.motor = motor
         self.registro = registro
